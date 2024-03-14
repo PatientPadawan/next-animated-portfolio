384e9a343b578c30391cfbbfbf70c6e5d3f104b3
PatientPadawan <49569154+PatientPadawan@users.noreply.github.com>
2024-03-14 12:38:10 -0700

    favicon location

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/images/icon.ico" sizes="any" />
+        <link rel="icon" href="/static/images/icon.ico" sizes="any" />
       </head>
       <body className={orbitron.className} style={background}>
         <TransitionProvider>{children}</TransitionProvider>
